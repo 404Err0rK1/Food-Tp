169d10142a5dd13d05f2d6df07d1caae429235cf
Ho.Tuan.Phat <404err0rk1@gmail.com>
2024-07-17 19:27:18 +0700

    update: api auth

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -10,7 +10,7 @@ import { MongoDBAdapter } from "@auth/mongodb-adapter"
 
 export const authOptions = {
   secret: process.env.SECRET,
-  // adapter: MongoDBAdapter(clientPromise),
+  adapter: MongoDBAdapter(clientPromise),
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID,
