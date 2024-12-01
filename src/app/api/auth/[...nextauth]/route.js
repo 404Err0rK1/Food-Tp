962e1a074c6723292f39bec7c50ebf863ccd4697
Ho.Tuan.Phat <404err0rk1@gmail.com>
2024-12-02 01:39:00 +0700

    fix auth

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -3,11 +3,10 @@ import { UserInfo } from "@/models/UserInfo";
 import bcrypt from "bcrypt";
 import * as mongoose from "mongoose";
 import { User } from '@/models/User';
-import NextAuth, { getServerSession } from "next-auth";
+import NextAuth from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials";
 import GoogleProvider from "next-auth/providers/google";
-import { MongoDBAdapter } from "@auth/mongodb-adapter"
-
+import { MongoDBAdapter } from "@auth/mongodb-adapter";
 
 export const authOptions = {
   secret: process.env.SECRET,
@@ -25,46 +24,23 @@ export const authOptions = {
         password: { label: "Password", type: "password" },
       },
       async authorize(credentials, req) {
-        // if(credentials){
-        // console.log(credentials) //check credentials
-        // }
         const email = credentials?.email;
         const password = credentials?.password;
 
-        mongoose.connect(process.env.MONGO_URL);
+        await mongoose.connect(process.env.MONGO_URL);
         const user = await User.findOne({ email });
-        
+
         const passwordOk = user && bcrypt.compareSync(password, user.password);
         if (passwordOk) {
-          console.log(user)
+          console.log(user);
           return user;
         }
-
-        return null
+        return null;
       }
     })
   ],
-
 };
-
-export async function isAdmin() {
-  const session = await getServerSession(authOptions);
-  const userEmail = session?.user?.email;
-
-  if (!userEmail) {
-
-    return false;
-  }
-  const userInfo = await UserInfo.findOne({ email: userEmail });
-  if (!userInfo) {
-
-    return false;
-  }
-  // console.log(userInfo.admin); //test value admin
-
-  return userInfo.admin;
-}
 
 const handler = NextAuth(authOptions);
 
-export { handler as GET, handler as POST }
+export { handler as GET, handler as POST };
